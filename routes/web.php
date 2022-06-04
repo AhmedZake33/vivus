636e2ee914a15ea26bbbf31c1c64627460ca13f7
ahmed zake <ahmed.zake333@gmail.com>
2022-06-04 18:49:03 +0200

    update

--- routes/web.php
+++ routes/web.php
@@ -23,21 +23,25 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 route::get('prepare',function(){
     DB::unprepared("
 
-    INSERT INTO model_has_roles (role_id, model_type, model_id) VALUES
-    (1, 'App\\Models\\User', 1);
+    INSERT INTO roles (id, name, guard_name, created_at, updated_at) VALUES
+    (1, 'admin', 'web', NULL, NULL);
 
     INSERT INTO permissions (id, name, guard_name, created_at, updated_at) VALUES
     (1, 'access_users', 'web', NULL, NULL),
     (3, 'admin_posts', 'web', NULL, NULL),
     (4, 'admin_categories', 'web', NULL, NULL);
 
-    INSERT INTO roles (id, name, guard_name, created_at, updated_at) VALUES
-    (1, 'admin', 'web', NULL, NULL);
-
+    
     INSERT INTO role_has_permissions (permission_id, role_id) VALUES
     (1, 1),
     (3, 1),
     (4, 1);
+
+    INSERT INTO model_has_roles (role_id, model_type, model_id) VALUES
+    (1, 'App\\Models\\User', 1);
+
+    
+
 
     ");
     return 'success';
